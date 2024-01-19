9ce2848c46e4924ee3f3b8d70bebfd79b7d78d58
Carl-Y <52552476+HH45137@users.noreply.github.com>
2024-01-19 17:13:54 +0800

    Update main.cpp

--- 数据结构CPP/main.cpp
+++ 数据结构CPP/main.cpp
@@ -11,7 +11,7 @@ bool isPalindrome(int x)
 
 	//输出每一个单独的数字位，如个十百千万的位置上相对应的数字
 	int t1 = 1, cur_r = 0, pre_r = 0;
-	for (size_t i = 1; i <= count; i++)
+	for (size_t i = count; i >= 1; i--)
 	{
 		//通过求余运算符逐步剔除高位
 		int v = (t1 *= 10);
@@ -19,11 +19,14 @@ bool isPalindrome(int x)
 
 		//通过减去上一个r得到当前r
 		cur_r = (r - pre_r) / (v / 10);
-		std::cout << cur_r << "\n";
+		//std::cout << cur_r << "\n";
 		pre_r = r;
 
 		//重新拼接颠倒的数字
-		num1 += cur_r * (v / 10);
+		int t2 = 1;
+		for (int j = 1; j < i; j++) { t2 *= 10; }
+		num1 += cur_r * t2;
+		//std::cout << num1 << "\n";
 	}
 
 	return false;
